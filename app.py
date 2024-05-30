60de887b20e9fe3a1d9e3a09c0abedec2e4d3a66
haplokion <googol502@gmail.com>
2024-05-30 12:22:00 +0300

    SQL-3

--- app.py
+++ app.py
@@ -18,20 +18,24 @@ login_manager.login_view = 'login'
 login_manager.login_message = 'Доступ к данной странице есть только у авторизованных пользователей '
 login_manager.login_message_category = 'warning'
 
-query = "show tables;"
+query = "DELETE FROM roles WHERE role_name = %s"
 
-def get_roles():
-    with db.connect().cursor(named_tuple=True) as cursor:
-            query = ('SELECT * FROM roles')
-            cursor.execute(query)
-            roles = cursor.fetchall()
-    return roles
+def send_query(query, *arg):
+    try:
+        with db.connect().cursor(named_tuple=True) as cursor:
+                cursor.execute(query, arg)
+                response = cursor.fetchall()
+                db.connect().commit()
+                print("SUCCESS")
+        return response
+    except mysql.connector.errors.DatabaseError as err:
+        db.connect().rollback()
+        print(f"ERROR: {err}")
+        return f"ERROR: {err}"
 
+role_name = "ТЕСТ"
 
 @app.route('/')
 def index():
-    with db.connect().cursor(named_tuple=True) as cursor:
-        cursor.execute(query)
-        roles = cursor.fetchall()
-        print(roles)
-        return roles
+    a = send_query(query, role_name)
+    return a

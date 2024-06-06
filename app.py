c025b935d1f896ce7529144bc7743a8996abc4a5
haplokion <googol502@gmail.com>
2024-06-06 23:20:00 +0300

    FORM

--- app.py
+++ app.py
@@ -21,6 +21,41 @@ from auth import bp as bp_auth, init_login_manager, checkRole
 app.register_blueprint(bp_auth)
 init_login_manager(app)
 
+#Жанры из таблицы genres
+def get_genres():
+    try:
+        with db.connect().cursor(named_tuple=True) as cursor:
+                query = ("SELECT * FROM genres")
+                cursor.execute(query)
+                genres = cursor.fetchall()
+                return genres
+    except Exception as err:
+        print(f"ERROR GET_GENRES: {err}")
+
+#Жанры книги из таблицы books_to_genres
+def get_book_genres(book_id):
+    try:
+        with db.connect().cursor(named_tuple=True) as cursor:
+                query = ("SELECT genre_id FROM books_to_genres WHERE book_id=%s")
+                cursor.execute(query, (book_id,))
+                genres_ids = cursor.fetchall()
+                return genres_ids
+    except Exception as err:
+        print(f"ERROR GET_BOOK_GENRES: {err}")
+
+#Запись в таблицу books_to_genres
+def set_book_genres(book_id, genre_ids):
+    try:
+        with db.connect().cursor(named_tuple=True) as cursor:
+                for genre_id in genre_ids:
+                    query = ("INSERT INTO books_to_genres (book_id, genre_id) VALUES (%s, %s)")
+                    cursor.execute(query, (book_id, genre_id))
+                db.connect().commit()
+                return 
+    except Exception as err:
+        db.connect().rollback()
+        print(f"ERROR SET_BOOK_GENRES: {err}")
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -38,4 +73,6 @@ def top_books():
 @login_required
 @checkRole("create")
 def create():
-    return render_template("create_book.html")
+    genres = get_genres()
+    print(genres)
+    return render_template("create_book.html", genres = genres)
--- test.py
+++ test.py
@@ -1,27 +1,42 @@
-from argon2 import low_level
-from hashlib import blake2b
+# from argon2 import low_level
+# from hashlib import blake2b
 
-def get_hash(login, password):
-    login = bytes(login, 'utf-8')
-    password = bytes(password, 'utf-8')
+# def get_hash(login, password):
+#     login = bytes(login, 'utf-8')
+#     password = bytes(password, 'utf-8')
 
-    #Соль формируется как хэш от логина и не хранится в ДБ
-    salt = blake2b(login).hexdigest()
-    #Преобразуется в байты
-    salt = bytes.fromhex(salt)
+#     #Соль формируется как хэш от логина и не хранится в ДБ
+#     salt = blake2b(login).hexdigest()
+#     #Преобразуется в байты
+#     salt = bytes.fromhex(salt)
 
-    #Хэш пароля длиной 16 байт = 32 HEX символа, мешается с солью
-    password_hash = low_level.hash_secret_raw(hash_len=16, salt=salt, time_cost=12, memory_cost=65536, parallelism=4, secret=password, type=low_level.Type.D)
-    password_hash = password_hash.hex()
-    return password_hash
+#     #Хэш пароля длиной 16 байт = 32 HEX символа, мешается с солью
+#     password_hash = low_level.hash_secret_raw(hash_len=16, salt=salt, time_cost=12, memory_cost=65536, parallelism=4, secret=password, type=low_level.Type.D)
+#     password_hash = password_hash.hex()
+#     return password_hash
 
-def check_hash(login, password, db_password_hash):
-    password_hash = get_hash(login, password)
-    if password_hash == db_password_hash:
-        return True
-    return False
+# def check_hash(login, password, db_password_hash):
+#     password_hash = get_hash(login, password)
+#     if password_hash == db_password_hash:
+#         return True
+#     return False
 
-login = input("Login\n")
-password = input("Password\n")
+# login = input("Login\n")
+# password = input("Password\n")
 
-print(get_hash(login, password))
+# print(get_hash(login, password))
+
+from app import db
+
+def set_book_genres(book_id, genre_ids):
+    try:
+        with db.connect().cursor(named_tuple=True) as cursor:
+                for genre_id in genre_ids:
+                    query = ("INSERT INTO books_to_genres (book_id, genre_id) VALUES (%s, %s)")
+                    cursor.execute(query, (book_id, genre_id))
+                print("SUCCESS")
+                db.connect().commit()
+                return 
+    except Exception as err:
+        db.connect().rollback()
+        print(f"ERROR GET_GENRES: {err}")
